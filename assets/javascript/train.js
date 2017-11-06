73056c9aeb96783f0067aa71a040ffed65197104
shulme801 <shulme801@gmail.com>
2017-11-06 18:11:14 -0500

    6 Nov initial commit

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCbmHrPPhiCuTuKBcLkWJcarLZKapvWNPU",
+    authDomain: "traintime-8a034.firebaseapp.com",
+    databaseURL: "https://traintime-8a034.firebaseio.com",
+    projectId: "traintime-8a034",
+    storageBucket: "",
+    messagingSenderId: "315737940391"
+  };
+  firebase.initializeApp(config);
+</script>
